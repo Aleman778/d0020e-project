958dd946375eac23376ee3ae37cfb949fcc3d58c
Aleman778 <alexander.mennborg@gmail.com>
2019-02-27 21:37:03 +0100

    Added maven packaging to project, and updated file structure.

---
+++ detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
@@ -0,0 +1,8 @@
+package eu.arrowhead.detector.provider;
+
+public class ObjectDetectionMain {
+
+    public static void main(String[]args){
+        System.out.println("Hello world object detection!");
+    }
+}
