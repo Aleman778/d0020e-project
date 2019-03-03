7a7ed6f15d8a2f92e3bf3baa03d2fbc5ae232e8a
KraftPunk <amajoh-6@student.ltu.se>
2019-03-03 18:59:11 +0100

    Object Detection Version 2.3 .
    Fixat så det är en mer korrekt JSON struktur.

--- detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
+++ detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
@@ -21,15 +21,15 @@ public class ObjectDetectionMain {
         String lidarData = "{\n\t\"LidarData\": {\n";
         for (int i = 0; i < data.size(); i++) {
             if (data.get(i).distance < ((calibration.get(i).distance) - margin)) {
-                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"TRUE\"";
+                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"TRUE\"\n\t\t}";
             } else {
-                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"FALSE\"";
+                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"FALSE\"\n\t\t}";
             }
             if((i+1) < data.size()){
                 lidarData = lidarData + ",\n";
             }
         }
-        lidarData = lidarData + "\n\t\t}\n\t}\n}";
+        lidarData = lidarData + "\n\t}\n}";
         System.out.println(lidarData);
     }
 
