4cf1308096cb7f7f42e6c82bc0653930985e0cc8
KraftPunk <amajoh-6@student.ltu.se>
2019-03-03 19:37:10 +0100

    Object Detection Version 2.0 .
    Nu kan man se vilken punkt/punkter som detekterade ett objekt.

--- detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
+++ detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
@@ -6,30 +6,32 @@ public class ObjectDetectionMain {
 
     private static int numPoints;
     private static double margin;
-    private static boolean isDetected;
     private static ArrayList<LidarPoint> calibration;
 
     //Setup for variables.
     private static void setup() {
         numPoints = 10;
         margin = 1.0;
-        isDetected = false;
         calibration = LidarGenerator.generate(numPoints);
     }
 
 
     //Checks if a datapoint in a LidarPoint list is less than the calibrated value for that datapoint
     private static void detection(ArrayList<LidarPoint> data) {
+        String lidarData = "{\n\t\"LidarData\": {\n";
         for (int i = 0; i < data.size(); i++) {
             if (data.get(i).distance < ((calibration.get(i).distance) - margin)) {
-                isDetected = true;
-                return;
+                lidarData = lidarData + "\t\t\"Datapoint " + data.get(i).angle + "\": \"TRUE\"";
+            } else {
+                lidarData = lidarData + "\t\t\"Datapoint " + data.get(i).angle + "\": \"FALSE\"";
+            }
+            if((i+1) < data.size()){
+                lidarData = lidarData + ",\n";
             }
         }
-        isDetected = false;
+        lidarData = lidarData + "\t}\n}";
+        System.out.println(lidarData);
     }
-
-    public static boolean detected(){ return isDetected;}
 
     public static void main(String args) {
         setup();
