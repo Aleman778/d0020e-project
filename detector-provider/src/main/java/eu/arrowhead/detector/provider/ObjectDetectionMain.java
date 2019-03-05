7553ff05ad0afc85c652112e76cb1e24e387173f
KraftPunk <amajoh-6@student.ltu.se>
2019-03-05 13:55:26 +0100

    Object Detection Version 2.3 .
    Fixat så det är en mer korrekt JSON struktur.

--- detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
+++ detector-provider/src/main/java/eu/arrowhead/detector/provider/ObjectDetectionMain.java
@@ -1,14 +1,26 @@
 package eu.arrowhead.detector.provider;
 
 import eu.arrowhead.lidar.common.LidarPoint;
-
 import java.util.ArrayList;
+import eu.arrowhead.common.api.ArrowheadApplication;
+import eu.arrowhead.common.api.ArrowheadSecurityContext;
+import eu.arrowhead.common.api.clients.core.ServiceRegistryClient;
+import eu.arrowhead.common.api.server.ArrowheadGrizzlyHttpServer;
+import eu.arrowhead.common.api.server.ArrowheadHttpServer;
+import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.common.misc.ArrowheadProperties;
+import eu.arrowhead.common.model.ServiceRegistryEntry;
 
-public class ObjectDetectionMain {
+
+public class ObjectDetectionMain extends ArrowheadApplication {
 
     private static int numPoints;
     private static double margin;
     private static ArrayList<LidarPoint> calibration;
+
+    public ObjectDetectionMain(String[] args) throws ArrowheadException {
+        super(args);
+    }
 
     //Setup for variables.
     private static void setup() {
@@ -17,15 +29,35 @@ public class ObjectDetectionMain {
         calibration = LidarGenerator.generate(numPoints);
     }
 
+    protected void onStart() throws ArrowheadException {
+        final ArrowheadProperties props = getProps();
+        if (props.getBooleanProperty("payload_from_file", false)) {
+            customResponsePayload = props.getProperty("custom_payload");
+        }
+        final ArrowheadSecurityContext securityContext = ArrowheadSecurityContext.createFromProperties(true);
+        final ArrowheadHttpServer server = ArrowheadGrizzlyHttpServer
+                .createFromProperties(securityContext)
+                .addResources(LidarGenerator.class, LidarPoint.class)
+                .addPackages("eu.arrowhead.demo", "eu.arrowhead.demo.provider.filter")
+                .start();
+
+        final ServiceRegistryClient registry = ServiceRegistryClient.createFromProperties(securityContext);
+        registry.register(ServiceRegistryEntry.createFromProperties(server));
+    }
+
+    protected void onStop() {
+
+    }
+
 
     //Checks if a datapoint in a LidarPoint list is less than the calibrated value for that datapoint
     private static void detection(ArrayList<LidarPoint> data) {
         String lidarData = "{\n\t\"LidarData\": {\n";
         for (int i = 0; i < data.size(); i++) {
             if (data.get(i).distance < ((calibration.get(i).distance) - margin)) {
-                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"TRUE\"\n\t\t}";
+                lidarData = lidarData + "\t\t\"Datapoint " + (i+1) + "\": {\n\t\t\t\"Angle\": \"" + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"TRUE\"\n\t\t}";
             } else {
-                lidarData = lidarData + "\t\t\"Datapoint " + i + "\": {\n\t\t\t\"Angle\": " + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"FALSE\"\n\t\t}";
+                lidarData = lidarData + "\t\t\"Datapoint " + (i+1) + "\": {\n\t\t\t\"Angle\": \"" + data.get(i).angle + "\"\n\t\t\t\"Detected\": \"FALSE\"\n\t\t}";
             }
             if((i+1) < data.size()){
                 lidarData = lidarData + ",\n";
@@ -37,9 +69,7 @@ public class ObjectDetectionMain {
 
     public static void main(String[] args) {
         setup();
-        while(true) {
-            detection(LidarGenerator.generate(numPoints));
-        }
+        new ObjectDetectionMain(args).start();
     }
 
 }
