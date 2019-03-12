83e49319d31e2020336f92d0d97c05540f38e825
RickardFrykgard <46930089+RickardFrykgard@users.noreply.github.com>
2019-03-12 15:31:52 +0100

    Update DetectionReadout.java

--- lidar-common/src/main/java/eu/arrowhead/lidar/common/DetectionReadout.java
+++ lidar-common/src/main/java/eu/arrowhead/lidar/common/DetectionReadout.java
@@ -1,6 +1,7 @@
 package eu.arrowhead.lidar.common;
 
 import java.time.Instant;
+import eu.arrowhead.lidar.common.ObjectDetection;
 
 public class DetectionReadout {
 
@@ -8,10 +9,22 @@ public class DetectionReadout {
     private int id;
     private double time;
 
-    public DetectionReadout(boolean isDetected, int id) {
-
-        this.isDetected = isDetected;
-        this.id = id;
+    public DetectionReadout() {
+        ObjectDetection object = new ObjectDetection();
+        this.isDetected = object.detection();
+        this.id = object.getId();
         this.time = Instant.now().getEpochSecond();
+    }
+    
+    public getId(){
+        return this.id;
+    }
+    
+    public getDetected(){
+        return this.isDetected;
+    }
+    
+    public getTime(){
+        return this.time;
     }
 }
